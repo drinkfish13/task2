910d4c19535e13bc26d6de539f61f5c6ceef1182
drinkfish13 <53922995+drinkfish13@users.noreply.github.com>
2019-10-16 00:24:43 -0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -3,7 +3,6 @@ new_list = []
 reoid = []
 for i in range(len(ids)):
     new_list += ids[i][1]
-n= new_list[0]
 for i in range(len(new_list)):
     if new_list[i] not in reoid:
         reoid.append(new_list[i])
